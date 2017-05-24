79bc89a266b84c2e7d138e521b951a2faf2f5ecf
Aizenberg12 <simonaizenberg@gmail.com>
2017-05-25 01:24:49 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+</head>
+<body>
+<!-- Шапка -->
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">LOGO</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#">Главная</a></li>
+                <li><a href="cabinet.html">Кабинет</a></li>
+                <li><a href="#">Выйти</a></li>
+                <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Поиск -->
+<form class="navbar-form navbar-left" role="search">
+    <div class="form-group">
+        <input type="text" class="form-control" placeholder="Поиск">
+    </div>
+    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+</form>
+<!-- Критерии поиска -->
+
+
+<!-- Отображение мероприятия -->
+ @foreach($events as $event)
+<div class="media">
+    <a class="pull-left" href="#">
+        <img class="media-object" src="{{asset($event->event_image)}}" alt="...">
+    </a>
+    <div class="media-body">
+        <div class='vevent'>
+            <p>
+            <h3>
+                <a class="summary" data-pjax-full="true" href="{{$event->link}}">{{$event->event_name}}</a>
+            </h3>
+<span class='dtstart' title='2017-05-16'>
+{{$event->event_data}}
+</span>
+<span class='location'>
+<a href="">{{$event->city}}</a>
+</span>
+<span class='category'>
+<a href='/it-vstrecha'>
+{{$event->event_category}}
+</a>
+</span>
+            </p>
+            <a class='icon_event_vip' href='#'></a>
+
+            <p>
+                Организатор:
+                <a href="/companies/42">{{$event->organizer}}</a>
+            </p>
+        </div>
+
+    </div>
+
+</div>
+@endforeach
+<!-- footer -->
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">Footer information</p>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+</body>
+</html>
