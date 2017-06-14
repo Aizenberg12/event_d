f82573337f5c11d545964123d2aae5f05c313c49
DarkPonny <Anton.kostuk.2012@gmail.com>
2017-06-15 02:24:07 +0300

    Лого, шрифты, изображения

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,9 +31,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                <a class="navbar-brand" href="{{route('homeShow')}}"><span class="glyphicon glyphicon-list-alt"></span> Make Event</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
